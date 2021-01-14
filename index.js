308dc12a0b8b5f28005f83f4cb29c840b8e450bd
Esiena Ekwofia <eekwofia@gmail.com>
2021-01-13 23:46:44 -0500

    was able to generate responses to question and create a file.

--- index.js
+++ index.js
@@ -1,38 +1,59 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-// TODO: Create an array of questions for user input
-const questions = [
+const util = require("util") //library
+const asyncWrite = util.promisify(fs.writeFile) //method like document.ready
 
+// TODO: Create an array of questions for user input
+const questions = (answers) =>
+`<h1> Hi! My name is ${answers.name} </h1>`
+
+inquirer
+.prompt([
     {
       type: 'input',
-      message: 'What is your user name?',
-      name: 'username',
+      message: 'What is the title of your project?',
+      name: 'name',
     },
     {
-      type: 'password',
-      message: 'What is your password?',
-      name: 'password',
+      type: 'input',
+      message: 'Enter a brief description of your project.',
+      name: 'description',
     },
     {
-      type: 'password',
-      message: 'Re-enter password to confirm:',
-      name: 'confirm',
+      type: 'input',
+      message: 'Enter your installation process.',
+      name: 'install',
     },
-  ]   
+    {
+        type: 'input',
+        message: 'Please describe the usage of this project.',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'Enter your installation process.',
+        name: 'install',
+      },
+    ]).then((answers) => {
+        const filename = `${answers.name}.md`;
+        return asyncWrite(filename, questions (answers) )}
+    ) .then (()=>{ console.log("Success!")}).catch ((err)=>{console.log(err)}) ;
+    // err ? console.log(err) : console.log("Success!")
+  
 
 // TODO: Create a function to write README file
-function writeFile(fileName, data) {
-    fs.writeFile(fileName, data);
-}
+// function writeFile(fileName, data) {
+//     fs.writeFile(fileName, data);
+// }
 
 // TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions).then((response) => {
-        console.log (response)
-        writeFile("README.md", generateReadMe())
-    })
-};
+// function init() {
+//     inquirer.prompt(questions).then((response) => {
+//         console.log (response)
+//         writeFile("README.md", generateReadMe())
+//     })
+// };
 
 // Function call to initialize app
-init();
+// init();
