72eac00dd3df02f8bb478613132087e83893964e
Esiena Ekwofia <eekwofia@gmail.com>
2021-01-13 22:16:47 -0500

    created a function to initialize app

--- index.js
+++ index.js
@@ -32,7 +32,11 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((response) => {
+        writeToFile("README.md", generateReadMe())
+    })
+};
 
 // Function call to initialize app
 init();
