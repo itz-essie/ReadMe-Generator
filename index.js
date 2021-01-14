0e3a51eabe0f9f61b056118c6a706b5bcfb1b7e7
Esiena Ekwofia <eekwofia@gmail.com>
2021-01-13 22:00:47 -0500

    formatted question with the proper syntax

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'username',
+    },
+    {
+      type: 'password',
+      message: 'What is your password?',
+      name: 'password',
+    },
+    {
+      type: 'password',
+      message: 'Re-enter password to confirm:',
+      name: 'confirm',
+    },
+  ]
+  .then((response) =>
+    response.confirm === response.password
+      ? console.log('Success!')
+      : console.log('You forgot your password already?!')
+  );    
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
