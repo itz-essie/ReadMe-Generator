c280b3fa65fff85bb58aeade537d92ff0dc0887e
Esiena Ekwofia <eekwofia@gmail.com>
2021-01-15 21:18:29 -0500

    completed list of questions

---
+++ Preview.md
@@ -0,0 +1,45 @@
+# ${answers.name}
+
+## Description
+
+${answers.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+### Installation
+
+```
+${answers.install}
+```
+
+### Usage
+
+${answers.usage}
+
+### License
+
+This project is licensed under an ${answers.License} license.
+
+## Contributing
+
+${answers.contributingGuidelines}
+
+## Tests
+
+To run test, run the following commands:
+
+```
+${answers.testInstructions}
+```
+
+## Questions
+
+- If you have any questions regarding this repository, contact me either by email at [Email](${answers.email}), or you can find more of my work within my github account attached below. Here, you can open an issue if needed.
+- [${answers.github}](https://github.com/${answers.github})
--- index.js
+++ index.js
@@ -1,51 +1,77 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-const util = require("util") //library
-const asyncWrite = util.promisify(fs.writeFile) //method like document.ready
+const util = require("util"); //library
+const asyncWrite = util.promisify(fs.writeFile); //method like document.ready
 
 // TODO: Create an array of questions for user input
-const questions = (answers) =>
-`<h1> Hi! My name is ${answers.name} </h1>`
+const questions = (answers) => `<h1> Hi! My name is ${answers.name} </h1>`;
 
 inquirer
-.prompt([
+  .prompt([
     {
-      type: 'input',
-      message: 'What is the title of your project?',
-      name: 'name',
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "gitHub",
     },
     {
-      type: 'input',
-      message: 'Enter a brief description of your project.',
-      name: 'description',
+      type: "input",
+      message: "what is your email address?",
+      name: "email",
     },
     {
-      type: 'input',
-      message: 'Enter your installation process.',
-      name: 'install',
+      type: "input",
+      message: "What is the title of your project?",
+      name: "name",
     },
     {
-        type: 'input',
-        message: 'Please describe the usage of this project.',
-        name: 'usage',
-      },
-      {
-        type: 'input',
-        message: 'Enter your installation process.',
-        name: 'install',
-      },
-    ]).then((answers) => {
-        const nameOfFile = `${answers.name}.md`;
-        return asyncWrite(nameOfFile, questions (answers) )}
-    ) .then (()=>{ console.log("Success!")}).catch ((err)=>{console.log(err)}) ;
-    // err ? console.log(err) : console.log("Success!")
-  
+      type: "input",
+      message: "Enter a brief description of your project.",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "Enter your installation process.",
+      name: "install",
+    },
+    {
+      type: "input",
+      message: "Please describe the usage of this project.",
+      name: "usage",
+    },
+    {
+      type: "list",
+      message: "What type of license should your project have?",
+      name: "license",
+      choices: ['GPLv3', 'MIT', 'Apache 2.0', 'BSD']
+    },
+    {
+      type: "input",
+      message: "Please describe any contributions. If not applicable please enter, N/A.",
+      name: "contributions",
+    },
+    {
+      type: "input",
+      message: "In order to run test, what command line shouls be run?",
+      name: "test",
+    },
+  ])
+  .then((answers) => {
+    const nameOfFile = `${answers.name}.md`;
+    return asyncWrite(nameOfFile, questions(answers));
+  })
+  .then(() => {
+    console.log("Success!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+// err ? console.log(err) : console.log("Success!")
 
 // TODO: Create a function to write README file
-// function writeFile(fileName, data) {
-//     fs.writeFile(fileName, data);
-// }
+function writeFile(fileName, data) {
+  fs.writeFile(fileName, data);
+}
 
 // TODO: Create a function to initialize app
 // function init() {
