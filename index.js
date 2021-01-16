3cf75651eb1a660d2c68a1e3c22d8eb89e8b2d6f
Esiena Ekwofia <eekwofia@gmail.com>
2021-01-16 00:22:07 -0500

    added psuedocode for if else statement

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const util = require("util"); //library
 const asyncWrite = util.promisify(fs.writeFile); //method like document.ready
 
 // TODO: Create an array of questions for user input
-const questions = (answers) => `# ${answers.title}
+const questions = (answers) => `# ${answers.title} + badge
 
 ## Description
 
@@ -34,7 +34,7 @@ ${answers.usage}
 
 This project is licensed under an ${answers.license} license.
 
-## Contributing
+## Contributions
 
 ${answers.contributions}
 
@@ -50,6 +50,7 @@ ${answers.tests}
 
 - If you have any questions regarding this repository, contact me either by email at **${answers.email}**, or you can find more of my work within my github account attached below: 
 **[${answers.gitHub}](https://github.com/${answers.gitHub})**.`
+
 
 function init(){
 inquirer
@@ -88,11 +89,11 @@ inquirer
       type: "list",
       message: "What type of license should your project have?",
       name: "license",
-      choices: ['GPLv3', 'MIT' + '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)', 'Apache 2.0', 'BSD']
+      choices: ['GPLv3', 'MIT', 'Apache 2.0', 'BSD']
     },
     {
       type: "input",
-      message: "Please describe any contributions. If not applicable please enter, N/A.",
+      message: "If applicable, provide guidelines on how other developers can contribute to your project. If not applicable please enter, N/A.",
       name: "contributions",
     },
     {
@@ -111,6 +112,17 @@ inquirer
   .catch((err) => {
     console.log(err);
   });
+  /* else if statement to print badge next to the title
+  if (choices === 'GPLv3'){
+  badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)'
+} else if (choices === 'MIT'){
+  badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+} else {
+  badge = '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)'
+}
+*/
+
+  
 // err ? console.log(err) : console.log("Success!")
 
 // TODO: Create a function to write README file
